3e46bd3d44197f7237e1a09a157debfaf34ae2ec
Almaz Khamidullin <almaz1@yandex.ru>
2022-06-20 23:13:25 +0300

    Gs device initial configuration.

--- target/linux/imx6ull/image/wirelessroad.mk
+++ target/linux/imx6ull/image/wirelessroad.mk
@@ -293,6 +293,25 @@ define Device/flexcan_wifi
 endef
 TARGET_DEVICES += flexcan_wifi
 
+define Device/gs
+        DEVICE_TITLE := Gs
+        DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-spi-dev kmod-can-flexcan kmod-wfx iwinfo wpad-mini
+        DEVICE_NAME := gs
+        DEVICE_DTS := gs
+        BOARDNAME := WIRELESSROAD_FLEXCAN_WIFI_IMX6ULL
+        SUPPORTED_DEVICES := wirelessroad_stream-imx6ull gs
+        IMAGE_SIZE := 31m
+        IMAGE_SIZE_FACTORY := 32m
+        CONSOLE := ttymxc0,115200
+        KERNEL := kernel-bin | buildDtb | append-dtb | uImage none | imx6ull-bootscript
+        IMAGES := u-boot.bin sdcard.bin mtd-sysupgrade.bin mtd-factory.bin
+        IMAGE/u-boot.bin := imx6ull-ubootimg
+        IMAGE/sdcard.bin := imx6ull-sdcard | append-metadata
+        IMAGE/mtd-sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | append-metadata | check-size $$$$(IMAGE_SIZE)
+        IMAGE/mtd-factory.bin := append-kernel | append-rootfs | pad-rootfs | imx6ull-mtd-factory | append-metadata | check-size $$$$(IMAGE_SIZE_FACTORY)
+endef
+TARGET_DEVICES += gs
+
 define Device/audio_stream_ethernet
 	DEVICE_TITLE := Audio Stream Ethernet
 	DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-spi-dev
