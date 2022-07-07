2f1fecd3a3e285b93dbf469353878682e86871d7
Sergey Moryakov <sergey@nqnet.org>
2022-07-08 02:37:53 +0300

    Prepare handling multiple meters

--- package/gs/gs/src/gs.c
+++ package/gs/gs/src/gs.c
@@ -4,17 +4,34 @@
 #include "libgs.h"
 
 
+gs_conninfo_t gs1_conninfo =
+{
+	.port = 4,
+	.baudrate = 9600,
+	.pinctrl = 4,
+	.rtspin = 8
+};
+
+gs_conninfo_t gs2_conninfo =
+{
+	.port = 5,
+	.baudrate = 9600,
+	.pinctrl = 4,
+	.rtspin = 0
+};
+
 /* jhust read all, print it and exit */
 int main(int argc, char* argv[])
 {
 	int ret;
-	// int i;
-	char port[] = "/dev/ttymxc5";
-	/* for now, hardcoded ttymxc2 , 115200 8n1 as device port */
-	gs_init(port);
-	gs_get_version();
-	gs_get_current_values();
-	gs_get_total_values();
-	gs_close();
+	modbus_t *ctx;
+	ctx = gs_init(&gs1_conninfo);
+	if (ctx != NULL)
+	{
+		gs_get_version(ctx);
+		gs_get_current_values(ctx);
+		gs_get_total_values(ctx);
+		gs_close(ctx);
+	}
 	return 0;
 }
