c53862a7a4886a286048916ad64018b30659ace3
Sergey Moryakov <sergey@nqnet.org>
2022-12-01 18:07:09 +0530

    WEBAPP: Fix file upluads and app restart code

--- package/ecb/webapp/src/www/deviceconfig.php
+++ package/ecb/webapp/src/www/deviceconfig.php
@@ -61,20 +61,20 @@ $macerror="";
         // Rename new config to App config
         rename("tmpConfig.ini","/usr/bin/AppConfig.ini");
         $output = shell_exec('sudo chmod 777 /usr/bin/AppConfig.ini');
-        $host    = "127.0.0.1";
-        $port    = 5500;
-        $message = "KILL";
-        echo "Message To server :".$message;
-        // create socket
-        $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-        // connect to server
-        $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
-        // send string to server
-        socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
-        // close socket
-        socket_close($socket);
+        // $host    = "127.0.0.1";
+        // $port    = 5500;
+        // $message = "KILL";
+        // echo "Message To server :".$message;
+        // // create socket
+        // $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+        // // connect to server
+        // $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+        // // send string to server
+        // socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+        // // close socket
+        // socket_close($socket);
         system('mv /usr/bin/aites.log /usr/bin/aites_last.log');
-        system('/usr/bin/baresip -f /etc/baresip >> /usr/bin/aites.log &');
+        system('/etc/init.d/baresip restart');
     } 
     $inputfile = file('/usr/bin/AppConfig.ini');
     $values = array();
--- package/ecb/webapp/src/www/fileupload.php
+++ package/ecb/webapp/src/www/fileupload.php
@@ -58,18 +58,18 @@ if ( !isset($_SESSION['username'])) {
     }
     if(isset($_GET['successBin'])){
         $binRet = "Successfully uploaded Firmware file ";
-        $host    = "127.0.0.1";
-        $port    = 5500;
-        $message = "KILL";
-        //echo "Message To server :".$message;
-        // create socket
-        $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-        // connect to server
-        $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
-        // send string to server
-        socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
-        // close socket
-        socket_close($socket);
+        // $host    = "127.0.0.1";
+        // $port    = 5500;
+        // $message = "KILL";
+        // //echo "Message To server :".$message;
+        // // create socket
+        // $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+        // // connect to server
+        // $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+        // // send string to server
+        // socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+        // // close socket
+        // socket_close($socket);
         // system('mv /usr/bin/aites.log /usr/bin/aites_last.log');
         $file_pointer = "/usr/bin/baresip"; 
         if (!unlink($file_pointer)) { 
