6337464ee99d540407e59d6e4226d44c379995d4
Sergey Moryakov <sergey@nqnet.org>
2022-12-02 16:08:51 +0530

    Webapp: fix fonts for all pafes

--- package/ecb/webapp/src/www/logserverconfig.php
+++ package/ecb/webapp/src/www/logserverconfig.php
@@ -79,7 +79,7 @@ if ( !isset($_SESSION['username'])) {
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="font-awesome.min.css">
 <link rel="stylesheet" href="aites.css">
 </head>
 <body>
